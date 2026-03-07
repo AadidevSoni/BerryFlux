2bbcdfab4e72b35d1b40f6704007abc8080dbfa2
Aadidev Soni <aadidevbahrain@gmail.com>
2026-03-07 12:15:19 +0530

    Remove build artifacts and add gitignore

--- build/CMakeFiles/Sandbox.dir/Sandbox/main.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Sandbox.dir/Sandbox/main.cpp.o: \
-  /Users/aadidev/Desktop/GameEngineDev/BerryFlux/Sandbox/main.cpp \
-  /Users/aadidev/Desktop/GameEngineDev/BerryFlux/Sandbox/../Engine/src/Engine.h
--- build/CMakeFiles/Sandbox.dir/Sandbox/src/Application.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Sandbox.dir/Sandbox/src/Application.cpp.o: \
-  /Users/aadidev/Desktop/GameEngineDev/BerryFlux/Sandbox/src/Application.cpp \
-  /Users/aadidev/Desktop/GameEngineDev/BerryFlux/Sandbox/src/../../Engine/src/Engine.h
